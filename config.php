2683589066b28c4b5c695b8585f375e44f3aab03
kottsin <66734606+k0ttsin@users.noreply.github.com>
2022-01-03 22:08:18 +0630

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+$db="test";
+$user="test";
+$pass="Password";
+$host="127.0.0.1";
+
+$connect=mysqli_connect($host,$user,$pass,$db);
+
+//Checking Database Connection;
+//if ($connect){
+//      echo "SUCCESS";
+//} 
+
+?>
